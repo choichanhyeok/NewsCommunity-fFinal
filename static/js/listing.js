48a441cc3cead6521acce9392dc914c8d962c335
hyeoKing <cksgurwkd12@naver.com>
2022-07-05 10:22:39 +0900

    FEAT: 뉴스 카드와 실제 뉴스 디테일 사이트 맵핑, 상세정보 리스팅

--- static/js/listing.js
+++ static/js/listing.js
@@ -7,16 +7,14 @@ const listing = () => {
     // index.html에서 적용돼, /news로 ajax를 호출해 네이버 뉴스 관련 정보를 수집해 리턴 //
     $.ajax({
         type: 'GET',
-        url: 'localhost:4943/news', // 테스트용 url
-//        url: 'http://awseb-AWSEB-1ILGAF6SPC5CX-485819853.ap-northeast-2.elb.amazonaws.com/news', // S3, cloudFront 배포용 url
+        url: '/api/news', // 테스트용 url
         data: {},
         success: function (response) {
-            let news_list = response['news_list'];
+            let news_list = response['body']['result']['newsTableList'];
             $('#cards-box').empty();
-
             for (let i = 0; i < news_list.length; i++) {
                 // 서버로 부터 받은 뉴스 리스트의 각 뉴스에 접근해 관련 정보를 받는다.
-                let post_id = news_list[i]['post_id'];
+                let post_id = news_list[i]['id'];
                 let title = news_list[i]['title'];
                 let contents = news_list[i]['explain']
                 let image_url = news_list[i]['image_url'];
@@ -27,7 +25,7 @@ const listing = () => {
                                             <img class="card-image-box" src=${image_url}
                                                  class="card-img-top">
                                             <div class="card-body">
-                                                <a href="/detail.html/${post_id}"><h5 class="card-title">${title}</h5></a>
+                                                <a href="/NewsCommunity-fFinal/detail.html?name=${post_id}"><h5 class="card-title">${title}</h5></a>
                                                 <p class="card-text">${contents}</p>
                                                 <p class="view">조회수: ${view}</p>
                                             </div>
