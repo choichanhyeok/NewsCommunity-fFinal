a85f5ab3924d039829166491e15f20eb0f048222
hyunzxn <jann1653@gmail.com>
2022-07-07 11:58:24 +0900

    Refactor: 모듈 만들기 일단 보류, 기존 정렬 함수 삭제

--- static/js/login.js
+++ static/js/login.js
@@ -8,7 +8,7 @@ $(document).ready(function () {
 			if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type) && !this.crossDomain) {
 				xhr.setRequestHeader("X-CSRFToken", csrftoken);
 			}
-			xhr.setRequestHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlbXkiLCJyb2xlcyI6WyJST0xFX1VTRVIiXSwiZXhwIjoxNjU3MDIwMjk3fQ.byJTlxPaaMUPur3P3gR3CqutS0viDcsKeqsx2cUI7EM");
+			xhr.setRequestHeader("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJlbXkiLCJyb2xlcyI6WyJST0xFX1VTRVIiXSwiZXhwIjoxNjU3MTYyOTY0fQ.dsyPymz5YJilDne8HsBR50tjS4KN7mw19B2UI3YTiI8");
 		}
 	});
 })
