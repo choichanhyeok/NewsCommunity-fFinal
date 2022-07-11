81305cfb673cb31863ed775548911fe6f9b42e25
Yeah-PC\blueflow <blueflow109@gmail.com>
2022-07-11 11:00:45 +0900

    Refactor: 로그아웃 url 변경

--- static/js/basic.js
+++ static/js/basic.js
@@ -50,7 +50,7 @@ function navIcon() {
 $(document).on("click", ".logout_icon", function signOut() {
 	$.ajax({
 		type: "GET",
-		url: '/api/token/signout',
+		url: '/api/user/signout',
 		data: {},
 		xhrFields: { withCredentials: true },
 		success: function (response) {
